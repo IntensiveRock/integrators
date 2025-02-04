51a35fd9e2d46b8cb8ac358a392c8ba4f684ca91
IntensiveRock <froggycham@gmail.com>
2025-02-04 15:10:15 -0700

    Forward Euler and other ODE solvers for vector valued function.

--- tests/solvetest.lisp
+++ tests/solvetest.lisp
@@ -26,9 +26,30 @@
 	(fe (make-instance 'integ:forward-euler))
 	(true-solution (make-array '(100) :initial-contents (loop for n from 0 below 10 by 0.1 collect (+ 1. (* 3. n))))))
     (setf solution (make-array '(100) :initial-contents (integ:solve problem fe)))
-   (> 0.00000001 (mse true-solution solution))))
+   (> 0.00000001 (mse true-solution solution))
+    ))
+
+(defun rk4-test ()
+  (let ((problem (make-instance 'ode:ode-problem :func #'(lambda (y_n t_n) 3) :y_0 1 :t-max 10 :t-step 0.1))
+	(fe (make-instance 'integ:rk4))
+	(true-solution (make-array '(100) :initial-contents (loop for n from 0 below 10 by 0.1 collect (+ 1. (* 3. n))))))
+    (setf solution (make-array '(100) :initial-contents (integ:solve problem fe)))
+   (> 0.00000001 (mse true-solution solution))
+    ))
+
+(defun heun-test ()
+  (let ((problem (make-instance 'ode:ode-problem :func #'(lambda (y_n t_n) 3) :y_0 1 :t-max 10 :t-step 0.1))
+	(fe (make-instance 'integ:heun))
+	(true-solution (make-array '(100) :initial-contents (loop for n from 0 below 10 by 0.1 collect (+ 1. (* 3. n))))))
+    (setf solution (make-array '(100) :initial-contents (integ:solve problem fe)))
+   (> 0.00000001 (mse true-solution solution))
+    ))
 
 (deftest integrator-test
-    (testing "Forward Euler Solver"
-	     (ok (fe-test))))
+  (testing "Forward Euler Solver"
+    (ok (fe-test)))
+  (testing "RK4 Test"
+    (ok (rk4-test)))
+  (testing "Heun Test"
+    (ok (heun-test))))
 (run-suite *package*)
