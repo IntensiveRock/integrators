3a2fe46f4d1aa049a7b5463dc0eef7e9fbe4ca66
IntensiveRock <froggycham@gmail.com>
2025-02-25 11:16:47 -0700

    Untracked .qlot

--- .qlot/dists/petalisp/software/petalisp-20241012-git/code/api/lazy.lisp
+++
@@ -1,6 +0,0 @@
-;;;; © 2016-2023 Marco Heisig         - license: GNU AGPLv3 -*- coding: utf-8 -*-
-
-(in-package #:petalisp.api)
-
-(defun lazy (function &rest arrays)
-  (petalisp.core:lazy-map 1 function (broadcast arrays)))
